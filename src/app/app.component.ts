cad579720cbd6cdd8a9467ad8b794e1e467a89ab
emrebondy <gucluemre976@gmail.com>
2025-03-18 10:38:56 +0100

    application fonctionnelle avec toutes les fonctionnalité

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,48 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+
+  constructor(private router: Router, private authService:AuthService) {}
+
+
+  // Vérifie si un rôle est présent dans le localStorage
+  isUserConnected(): boolean {
+    const role = sessionStorage.getItem('role'); // Utilisation de sessionStorage
+    console.log("dans isUserCONNECTED : " + role);
+    return role !== null;
+  }
+
+  // Redirige vers la page de connexion
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  // Change de rôle (par exemple, admin -> user ou inversement)
+  changeRole(): void {
+    const currentRole = sessionStorage.getItem('role');
+    if (currentRole === 'admin') {
+        sessionStorage.setItem('role', 'user');
+    } else {
+        sessionStorage.setItem('role', 'admin');
+    }
+    // Optionnellement, recharger la page pour appliquer les changements immédiatement
+    window.location.reload();
+}
+
+// Déconnexion de l'utilisateur
+logout(): void {
+    sessionStorage.removeItem('role');
+    this.router.navigate(['/login']);  // Redirige vers la page de connexion
+}
+
 
 }
